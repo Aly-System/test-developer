1fe5fb167fbb9ea6ad29fa08c46175b657e2d491
kaliss96 <aguirretellez96@gmail.com>
2021-03-27 17:29:45 -0600

    Add files via upload

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { UserScreen } from '../components/users/UserScreen';
-import { DcScreen } from '../components/dc/DcScreen';
 import { SearchScreen } from '../components/search/SearchScreen';
 
 export const DashboardRoutes = () => {
@@ -11,7 +10,6 @@ export const DashboardRoutes = () => {
             <div className="container mt-2">
                 <Switch>
                     <Route exact path="/user/:userId" component={ UserScreen } />
-                    <Route exact path="/dc" component={ DcScreen } />
                     <Route exact path="/search" component={ SearchScreen } />
 
                     <Redirect to="/search" />
